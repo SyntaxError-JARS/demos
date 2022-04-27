8d0d50f7cf0a5c129a5173c1cda10b5048835079
JesterCharles <JesterCharles@gmail.com>
2022-04-27 12:32:49 -0400

    Added in new joins and views to the sql pokedex script and added some additional challenges

--- sql/pokedex-script.sql
+++ sql/pokedex-script.sql
@@ -21,11 +21,17 @@ CREATE table pokemon (
 	hp int not null,
 	atk int not null,
 	element_type varchar(50) not null,
-	ability_1 varchar(25),
-	constraint fk_abilities foreign key(ability_1) references abilities(ability_name)
+	ability_1 varchar(25)
+	--constraint fk_abilities foreign key(ability_1) references abilities(ability_name)
 );
 
+-- The best and most useful case for alter
+alter table pokemon
+add constraint fk_abilities 
+foreign key(ability_1) references abilities(ability_name);
+
 drop table pokemon;
+drop table abilities;
 
 -- BAD PRACTICE to have two values in a single element
 insert into pokemon 
@@ -38,7 +44,7 @@ values
 -- Now references foreign key ability name scratch
 insert into pokemon 
 values
-(default, 'squirtle', '30', '3', 'water', 'Scratch');
+(default, 'charmander', '25', '5', 'fire', 'Scratch');
 
 -- Now references a non-existent ability
 insert into pokemon 
@@ -49,8 +55,58 @@ insert into pokemon
 values
 (default, 'squirtle', '30', '3', 'water', 'Scratch');
 
+insert into pokemon 
+values
+(default, 'pikachu', '15', '10', 'lightning', 'Tackle');
+
 select pokemon_name, abilities from pokemon p ;
+
+-- multi-insert 
+insert into pokemon 
+values
+(default, 'rattata', '12', '2', 'normal', 'Scratch'),
+(default, 'pidgey', '10', '1', 'normal', 'Tackle');
+
+-- bad
+insert into pokemon (pokemon_name, hp, atk, ability_1, element_type )
+values
+('rattata', '12', '2', 'normal', 'Scratch'),
+('pidgey', '10', '1', 'normal', 'Tackle');
+
+insert into pokemon (pokemon_name, hp, atk, element_type, ability_1 )
+values
+('rattata', '12', '2', 'normal', 'Scratch'),
+('pidgey', '10', '1', 'normal', 'Tackle');
 
 
 
 select * from pokemon p ;
+select * from abilities a ;
+
+-- joins
+
+select pokemon.pokemon_name, pokemon.ability_1, pokemon.atk, abilities.atk_multiplier, abilities.dmg_type 
+from pokemon 
+join abilities ON pokemon.ability_1 = abilities.ability_name;
+
+
+-- Same exact results as above, but we used the alias instead (pokemon p and ablities a)
+-- If something is aliased, you have to use that
+select p.pokemon_name, p.ability_1, p.atk, a.atk_multiplier, a.dmg_type 
+from pokemon p
+join abilities a ON p.ability_1 = a.ability_name;
+
+
+-- views
+
+create view pokemon_abilities as
+select p.pokemon_name, p.ability_1, p.atk, a.atk_multiplier, a.dmg_type 
+from pokemon p
+join abilities a ON p.ability_1 = a.ability_name;
+
+select * from pokemon_abilities;
+
+-- fun things to do on your own with this. Figure out how you might find their attack dmg total
+-- mess with some more views 
+-- add a second ability
+-- think is there any other redundant info
