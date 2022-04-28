26e8492b28333386986fb87f1e0a4483ee77517a
JesterCharles <JesterCharles@gmail.com>
2022-04-28 10:59:24 -0400

    updated the sql pokedex script to be executable by all, given they have the table names available

--- sql/pokedex-script.sql
+++ sql/pokedex-script.sql
@@ -1,113 +1,147 @@
--- Create ourselves a schema	
-create schema pokedex;
+-----------------------------------------
+-------------C(reate)RUD-----------------
+-- NOTE: MAKE SURE YOUR SCHEMA CONTAINS NONE OF THE FOLLOWING TABLE NAMES
+	-- elemental_type
+	-- abilites
+	-- pokemon
+-- Create ourselves a schema, un comment below command if you need to create it, 
+-- then comment out again and execute script after selecting the appropriate schema in the toolbar above
+-- create schema pokedex;
+
+-- CREATE OUR TABLES & APPLY CONSTRAINTS
+
+-- Need this incase we add any information pertaining to the element type and insuring consistency
+create table elemental_type (
+	id serial primary key,
+	"type" varchar(30)
+);
 
 create table abilities (
 	ability_name varchar(25) primary key,
 	atk_multiplier int,
-	dmg_type varchar(30)
+	dmg_type int
 );
 
-insert into abilities
-values
-('Scratch', 2, 'normal');
+alter table abilities
+add constraint fk_elemental_type
+foreign key(dmg_type) references elemental_type(id);
 
-insert into abilities
-values
-('Tackle', 3, 'normal');
-
-CREATE table pokemon (
+create table pokemon (
 	id serial primary key,
 	pokemon_name varchar(50),
 	hp int not null,
 	atk int not null,
-	element_type varchar(50) not null,
-	ability_1 varchar(25)
-	ability_dmg int 
+	element_type int not null,
+	ability1 varchar(25) not null,
+	ability2 varchar(25) not null check (ability1 != ability2)
 	--constraint fk_abilities foreign key(ability_1) references abilities(ability_name)
 );
 
 -- The best and most useful case for alter
 alter table pokemon
-add constraint fk_abilities 
-foreign key(ability_1) references abilities(ability_name);
+add constraint fk_abilities_1
+foreign key(ability1) references abilities(ability_name);
 
-drop table pokemon;
-drop table abilities;
+alter table pokemon
+add constraint fk_abilitie_2 
+foreign key(ability2) references abilities(ability_name);
 
--- BAD PRACTICE to have two values in a single element
+alter table pokemon
+add constraint fk_elemental_type 
+foreign key(element_type) references elemental_type(id);
+
+------------------------------------
+-- Uncomment and Only execute if you need to dropp
+-- drop table pokemon; 
+-- drop table abilities;
+-- drop table elemental_type;
+------------------------------------
+
+-- CREATING NEW ENTRIES
+-- single insert type first
+insert into elemental_type 
+values
+(default, 'normal');
+
+insert into elemental_type 
+values
+(default, 'fire'),
+(default, 'water'),
+(default, 'lightning'),
+(default, 'grass');
+
+-- single insert
+insert into abilities
+values
+('Scratch', 2, 1);
+
+-- multi-insert
+insert into abilities
+values
+('Tackle', 3, 1),
+('Fire Breath', 3, 2),
+('Water Gun', 5, 3),
+('Shock', 3, 4),
+('Vine Whip', 3, 5);
+
+
+
+-- single insert into pokemon
 insert into pokemon 
 values
-(default, 'charmander', '25', '5', 'fire', 'Scratch,Fire Breath');
-insert into pokemon 
-values
-(default, 'squirtle', '30', '3', 'water', 'Scratch,Water Gun');
-
--- Now references foreign key ability name scratch
-insert into pokemon 
-values
-(default, 'charmander', '25', '5', 'fire', 'Scratch');
-
--- Now references a non-existent ability
-insert into pokemon 
-values
-(default, 'bulbasaur', '22', '6', 'grass', 'Tackle');
-
-insert into pokemon 
-values
-(default, 'squirtle', '30', '3', 'water', 'Scratch');
-
-insert into pokemon 
-values
-(default, 'pikachu', '15', '10', 'lightning', 'Tackle');
-
-select pokemon_name, abilities from pokemon p ;
+(default, 'rattata', '12', '2', 1, 'Scratch', 'Tackle');
 
 -- multi-insert 
 insert into pokemon 
 values
-(default, 'rattata', '12', '2', 'normal', 'Scratch'),
-(default, 'pidgey', '10', '1', 'normal', 'Tackle');
+(default, 'pidgey', '10', '1', 1, 'Tackle', 'Scratch'),
+(default, 'charmander', '25', '5', 2, 'Scratch', 'Fire Breath'),
+(default, 'squirtle', '30', '3', 3, 'Scratch', 'Water Gun'),
+(default, 'pikachu', '15', '10', 4, 'Tackle', 'Shock'),
+(default, 'bulbasaur', '22', '6', 5, 'Tackle', 'Vine Whip');
 
--- bad
-insert into pokemon (pokemon_name, hp, atk, ability_1, element_type )
-values
-('rattata', '12', '2', 'normal', 'Scratch'),
-('pidgey', '10', '1', 'normal', 'Tackle');
+-------------Create End------------------
 
-insert into pokemon (pokemon_name, hp, atk, element_type, ability_1 )
-values
-('rattata', '12', '2', 'normal', 'Scratch'),
-('pidgey', '10', '1', 'normal', 'Tackle');
-
-
-
+-----------------------------------------
+---------------CR(ead)UD-----------------
 select * from pokemon p ;
 select * from abilities a ;
 
 -- joins
 
-select pokemon.pokemon_name, pokemon.ability_1, pokemon.atk, abilities.atk_multiplier, abilities.dmg_type 
+select pokemon.pokemon_name, pokemon.ability1, pokemon.atk, abilities.atk_multiplier, abilities.dmg_type 
 from pokemon 
-join abilities ON pokemon.ability_1 = abilities.ability_name;
+join abilities ON pokemon.ability1 = abilities.ability_name;
 
 
 -- Same exact results as above, but we used the alias instead (pokemon p and ablities a)
 -- If something is aliased, you have to use that
-select p.pokemon_name, p.ability_1, p.atk, a.atk_multiplier, a.dmg_type 
+select p.pokemon_name, p.ability1, p.atk, a.atk_multiplier, a.dmg_type 
 from pokemon p
-join abilities a ON p.ability_1 = a.ability_name;
+join abilities a ON p.ability1 = a.ability_name;
 
 
 -- views
 
 create view pokemon_abilities as
-select p.pokemon_name, p.ability_1, p.atk, a.atk_multiplier, a.dmg_type 
+select p.pokemon_name, p.ability1, p.atk, a.atk_multiplier, a.dmg_type 
 from pokemon p
-join abilities a ON p.ability_1 = a.ability_name;
+join abilities a ON p.ability1 = a.ability_name;
 
 select * from pokemon_abilities;
 
--- fun things to do on your own with this. Figure out how you might find their attack dmg total
--- mess with some more views 
--- add a second ability
--- think is there any other redundant info
+-------------Read End--------------------
+
+-----------------------------------------
+-------------CRU(pdate)D-----------------
+
+
+
+-------------Update End------------------
+
+-----------------------------------------
+-------------CRUD(elete)-----------------
+
+
+
+-------------Delete End------------------
